4fc2da05ed8b3ed64d83e6bbffba366ade0b022c
Jorge <jorgealberto.lopezhdz@gmail.com>
2024-08-23 18:06:48 -0600

    Initial commit

---
+++ lib/login/login.dart
@@ -0,0 +1,3 @@
+export 'view/login_form.dart';
+export 'view/login_page.dart';
+export 'bloc/login_bloc.dart';
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_bloc/app.dart';
+
+void main() => runApp(const App());
